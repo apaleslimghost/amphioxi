f66590bc75cabfeb34bbcc1bcb98ffa364a4bca8
Kara Brightwell <kara@153.io>
2025-12-31 08:31:36 +0000

    vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-})
+export default defineConfig(({ command }) => ({
+	plugins: [preact()],
+	base: command === "serve" ? "/" : "/bard",
+}));
